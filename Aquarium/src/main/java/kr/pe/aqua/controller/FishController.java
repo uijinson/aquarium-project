e8a73026ff698f5548f3f2a558a116cc5b4c1307
melody <thgilnoomelody@gmail.com>
2020-09-07 02:37:20 +0900

    modify repository

--- Aquarium/src/main/java/kr/pe/aqua/controller/FishController.java
+++ Aquarium/src/main/java/kr/pe/aqua/controller/FishController.java
@@ -1,11 +1,11 @@
 package kr.pe.aqua.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +21,7 @@ import kr.pe.aqua.model.Fish;
 import kr.pe.aqua.model.FishRepository;
 
 @RestController
-@Api(value="test controller specification v1")
+@Api(value="FishController specification v1")
 @RequestMapping("/api")
 public class FishController {
 
@@ -30,70 +30,47 @@ public class FishController {
 		this.repository = repository;
 	}
 	
-//	@ApiResponses({
-//		@ApiResponse(code = 200, message = "맞아요!"),
-//		@ApiResponse(code = 500, message = "서버 에러"),
-//		@ApiResponse(code = 403, message = "금지"),
-//		@ApiResponse(code = 404, message = "어디있지?"),
-//		@ApiResponse(code = 401, message = "허가노노") })
-//	
-	
-//	@ApiOperation(value="all() 메소드 사용", notes="모든 물고기 조회")
-//	@GetMapping("/fishes")
-//	public List<Fish> all() {
-//		return repository.findAll();
-//	}
-//	@ApiOperation(value="newFish() 메소드 사용", notes="새로운 물고기 추가")
-//	@PostMapping("/fishes")
-//	public Fish newFish(@ApiParam(value = "물고기 이름을 지어주세요", required = true) @RequestParam String fishName,
-//						@ApiParam(value = "물고기를 소개해주세요", required= true) @RequestParam String fishExplain) {
-//		Fish newFish = Fish.builder().fishName(fishName).fishExpain(fishExplain).build();
-//		return repository.save(newFish);
-//	}
-//
-//	@ApiOperation(value="Fish one() 메소드 사용", notes="물고기 한 마리 검색")
-//	@GetMapping("/fishes/{id}")
-//	// @PathVariable Long id
-//	public Fish one(@ApiParam(value="fish번호", required = true) @RequestParam Long no) {
-//
-//		return repository.findById(no).orElseThrow(() -> new FishNotFoundException(no));
-//	}
-//	
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "맞아요!"),
+		@ApiResponse(code = 500, message = "서버 에러"),
+		@ApiResponse(code = 403, message = "금지"),
+		@ApiResponse(code = 404, message = "어디있지?"),
+		@ApiResponse(code = 401, message = "허가노노") })
 	
 	
-//	@ApiOperation(value="replaceFish() 메소드 사용", notes="기존 물고기 수정")
-//	@PutMapping("/fishes/{no}")
-//	public Fish replaceFish(@ApiParam(value = "물고기 이름을 지어주세요", required = true) @RequestParam String fishName,
-//							@ApiParam(value = "물고기를 소개해주세요", required= true) @RequestParam String fishExplain,
-//							@ApiParam(value = "fish번호", required=true) @RequestParam Long no) {
-//		Fish newFish = Fish.builder().fishName(fishName).fishExpain(fishExplain).build();
-//		return repository.findById(no).map(fish -> {
-////			fish.setFid(newFish.getFid());
-////			fish.setPw(newFish.getPw());
-//			fish.setFishName(newFish.getFishName());
-//			fish.setFishExpain(newFish.getFishExpain());
-//			return repository.save(fish);
-//		}).orElseGet(() -> {
-////			newFish.setNo(no);
-//			return repository.save(newFish);
-//		});
-//	}
-//	@ApiOperation(value="deleteFish() 메소드 사용", notes="물고기 삭제")
-//	@DeleteMapping("/fishes/{no}")
-//	public void deleteFish(@ApiParam(value="fish번호", required = true) @RequestParam Long no) {
-//		repository.deleteById(no);
-//	}
-//	
-//	//? 사용자 정의 쿼리 메소드 중 이름이 일치되는 동명 이인들 검색해서 출력해보기
-//	@GetMapping("/findbyname/{fishName}")
-//	public List<Fish> getFishesName(@PathVariable String fishName) {
-//		return repository.findFishByFishName(fishName);
-//	}
-//	@GetMapping("/findbynamecontaining/{fishName}")
-//	public List<Fish> getFishContaining(@PathVariable String fishName) {
-//		return repository.findByFishNameContaining(fishName);
-//	}
-//	
+	@ApiOperation(value="newFish() 메소드 사용", notes="새로운 물고기 추가")
+	@GetMapping("/fishes")
+	public Fish newFish(@ApiParam(value = "물고기 이름을 지어주세요", required = true) @RequestParam String fishName,
+						@ApiParam(value = "물고기를 소개해주세요", required= true) @RequestParam String fishExplain) {
+		Fish newFish = Fish.builder().fishName(fishName).fishExplain(fishExplain).build();
+		return repository.save(newFish);
+	}
+
+	@ApiOperation(value="Fish one() 메소드 사용", notes="물고기 한 마리 검색")
+	@GetMapping("/fishes/{fishId}")
+	public Fish oneFish(@ApiParam(value="fish번호", required = true) @RequestParam Long fishId) {
+
+		return repository.findById(fishId).orElseThrow(() -> new FishNotFoundException(fishId));
+	}
+	
+	@ApiOperation(value="replaceFish() 메소드 사용", notes="기존 물고기 수정")
+	@PutMapping("/fishes/{fishId}")
+	public Optional<Object> replaceFish(@ApiParam(value = "물고기 이름을 지어주세요", required = false) @RequestParam String fishName,
+							@ApiParam(value = "물고기를 소개해주세요", required= false) @RequestParam String fishExplain,
+							@ApiParam(value = "fish번호", required=true) @RequestParam Long fishId) {
+		Fish newFish = Fish.builder().fishName(fishName).fishExplain(fishExplain).build();
+		return repository.findById(fishId).map(fish -> {
+			fish.setFishName(newFish.getFishName());
+			fish.setFishExplain(newFish.getFishExplain());
+			return repository.save(fish);
+		});
+	}
+	
+	@ApiOperation(value="deleteFish() 메소드 사용", notes="물고기 삭제")
+	@DeleteMapping("/fishes/{fishId}")
+	public void deleteFish(@ApiParam(value="fish번호", required = true) @RequestParam Long fishId) {
+		repository.deleteById(fishId);
+	}
 	
 }
 
--- Aquarium/src/main/java/kr/pe/aqua/model/Fish.java
+++ Aquarium/src/main/java/kr/pe/aqua/model/Fish.java
@@ -5,7 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,22 +19,33 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
-
+@SequenceGenerator(name = "FISH_SEQ_GEN", sequenceName="FISH_SEQ", initialValue = 1, allocationSize = 1)
 public class Fish {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-					generator="FISH_SEQ_GEN")
-	private Long fishId; //pk
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FISH_SEQ_GEN")
+	private Long fishId; // pk
 	@Column(unique = true, nullable = false)
 	private String fishName;
 	@Column(unique = true, nullable = false)
-	private String fishExpain;
+	private String fishExplain;
 	
+	private int fishPrice;
+	private int fishHp;
+	private String fishImg; //상점에 들어갈 사진 
+	private String  fishSrc; //obj의 경로
+
 	@Builder
-	public Fish(Long fishId, String fishName, String fishExpain) {
+	public Fish(Long fishId, String fishName, String fishExplain, int fishPrice, int fishHp, String fishImg,
+			String fishSrc) {
 		super();
 		this.fishId = fishId;
 		this.fishName = fishName;
-		this.fishExpain = fishExpain;
+		this.fishExplain = fishExplain;
+		this.fishPrice = fishPrice;
+		this.fishHp = fishHp;
+		this.fishImg = fishImg;
+		this.fishSrc = fishSrc;
 	}
+	
+
 }
